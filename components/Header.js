bb492931f6da9b0fd353eb38663f8c8603bc4f50
Fuad Olaleye <heniolarr@gmail.com>
2024-01-04 17:31:49 +0100

    Fixed Font

--- components/Header.js
+++ components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
     return (
         <div className={`${manrope.className} h-fit bg-pink px-[25px] pt-[50px] pb-[40px] relative`}>
             <h2 className='tracking-wide font-bold text-sm'>OLALEYE FUAD  |  FULLSTACK DEVELOPER</h2>
-            <h1 className={`${syne.className} title tracking-tighter uppercase text-[36px]`}>Unleashing creativity on the web &mdash; through code.</h1>
+            <h1 className={`${syne.className} title tracking-tighter uppercase text-[28px] md:text-[36px]`}>Unleashing creativity on the web &mdash; through code.</h1>
             <div className='border-l-2 border-black px-6 py-0 max-w-md text-sm font-regular h-fit mt-4'>
                 <p className='text-sm font-regular'>
                     Taking development beyond the scope of coding, I have built problem solving projects and worked with clients to transform their brands.
